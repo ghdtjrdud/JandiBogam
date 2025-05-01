ff5c526a206c5a832c441a2fb7f59da663ed8940
ghdtjrdud38@gmail.com <ghdtjrdud38@gmail.com>
2025-05-01 15:48:56 +0900

    프로젝트 생성

---
+++ JandIBogam/src/main/java/com/ssafy/jandibogam/JandIBogamApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.jandibogam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JandIBogamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JandIBogamApplication.class, args);
+    }
+
+}
---
+++ JandIBogam/src/test/java/com/ssafy/jandibogam/JandIBogamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.jandibogam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JandIBogamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
