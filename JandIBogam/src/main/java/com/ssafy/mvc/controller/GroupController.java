efcb219b4d3ac773fa8160df824ea514c5fad9b8
ghdtjrdud38@gmail.com <ghdtjrdud38@gmail.com>
2025-05-18 14:45:21 +0900

    GPT AI 초기설정

--- JandIBogam/src/main/java/com/ssafy/mvc/controller/GroupController.java
+++ JandIBogam/src/main/java/com/ssafy/mvc/controller/GroupController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/groups")
+@RequestMapping("/api/groups")
 public class GroupController {
 
     private final GroupService groupService;
