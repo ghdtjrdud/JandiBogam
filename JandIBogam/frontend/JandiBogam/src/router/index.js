43091c196a05a1ebe56be23354a718d00f586344
ghdtjrdud38@gmail.com <ghdtjrdud38@gmail.com>
2025-05-20 21:17:55 +0900

    충돌커밋

--- JandIBogam/frontend/JandiBogam/src/router/index.js
+++ JandIBogam/frontend/JandiBogam/src/router/index.js
@@ -4,7 +4,6 @@ import LoginView from '../views/LoginView.vue'
 import SignupView from '../views/SignupView.vue'
 //import FindCredentialsView from '../views/FindCredentialsView.vue'
 
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -16,21 +15,25 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+
       component: () => import('../views/AboutView.vue'),
     },
     {
-    path: '/login',
-    name: 'login',
-    component: LoginView,
-  },
-  {
-    path: '/signup',
-    name: 'signup',
-    component: SignupView,
-  }
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignupView,
+    },
+    {
+      path: '/mypage',
+      name: 'mypage',
+      // 동적 import 방식(권장)
+      component: () => import('../views/MyPageView.vue'),
+    },
   ],
 })
 
--- JandIBogam/src/main/java/com/ssafy/mvc/model/dto/NutrientIdNameDto.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.mvc.model.dto;
-
-import lombok.Data;
-
-@Data
-public class NutrientIdNameDto {
-    private String name;
-    private int id;
-}
